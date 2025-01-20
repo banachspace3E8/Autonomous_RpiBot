1593328b9962f797fa3ac4c6e5972dc14ce453f0
banachspace3E8 <banachspace3e8@gmail.com>
2025-01-20 17:18:15 -0500

    Moved scripts to right subfolder

---
+++ rpi_bot/scripts/motor_pwm.py
@@ -0,0 +1,16 @@
+#Shishir Khanal
+#1/19/2025
+#Pwm for motor
+
+import RPi.GPIO as GPIO
+import time
+
+RtMtr_A = 24
+RtMtr_A = 23
+RtMtr_en = 25
+
+LftMtr_A = 14
+LftMtr_A = 15
+LftMtr_en = 4
+
+GPIO.setmode(GPIO.BCM)
